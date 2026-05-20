7dfe655f7dd073d4e09d893b2ad14a36ec1e1f44
Harshill Tyagi <82799112+harshilltyagi@users.noreply.github.com>
2026-05-20 21:35:51 +0530

    Update config.js
    update base url

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: "https://reddit-clone-8vrm.onrender.com",
+  BASE_URL: "https://reddit-clone-8vrm.onrender.com/api",
   APP_NAME: "Reddit Clone",
   TOKEN_KEY: "token",
   USER_KEY: "user",
